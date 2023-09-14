0f96acf26e8e6fd164cb17e0d02a1b863dc500be
Nadia <nadiahosisy@gmail.com>
2023-09-14 12:15:22 +0300

    add ProtectedRoute component

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,14 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children, user }) => {
-  
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    }
+  }, [navigate, user]);
+
+  return <>{children}</>;
 };
 
 export default ProtectedRoute;
